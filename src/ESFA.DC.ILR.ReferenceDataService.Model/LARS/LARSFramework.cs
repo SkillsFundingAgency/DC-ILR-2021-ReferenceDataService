290e6b137d7dcde29f438c2cd13aedc439ce571c
Ashley Beecham <ash@momentoussolutions.co.uk>
2019-02-08 16:31:08 +0000

    68999 - relationship of Framework and Framework Aims corrected.

--- src/ESFA.DC.ILR.ReferenceDataService.Model/LARS/LARSFramework.cs
+++ src/ESFA.DC.ILR.ReferenceDataService.Model/LARS/LARSFramework.cs
@@ -5,6 +5,12 @@ namespace ESFA.DC.ILR.ReferenceDataService.Model.LARS
 {
     public class LARSFramework
     {
+        public int FworkCode { get; set; }
+
+        public int ProgType { get; set; }
+
+        public int PwayCode { get; set; }
+
         public DateTime? EffectiveFrom { get; set; }
 
         public DateTime? EffectiveTo { get; set; }
--- src/ESFA.DC.ILR.ReferenceDataService.Model/LARS/LARSFrameworkAim.cs
+++ src/ESFA.DC.ILR.ReferenceDataService.Model/LARS/LARSFrameworkAim.cs
@@ -5,19 +5,13 @@ namespace ESFA.DC.ILR.ReferenceDataService.Model.LARS
 {
     public class LARSFrameworkAim
     {
-        public int FworkCode { get; set; }
-
-        public int ProgType { get; set; }
-
-        public int PwayCode { get; set; }
-
         public int? FrameworkComponentType { get; set; }
 
         public DateTime EffectiveFrom { get; set; }
 
         public DateTime? EffectiveTo { get; set; }
 
-        public List<LARSFramework> LARSFrameworks { get; set; }
+        public LARSFramework LARSFramework { get; set; }
 
         public List<LARSFrameworkApprenticeshipFunding> LARSFrameworkApprenticeshipFundings { get; set; }
     }
