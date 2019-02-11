a3087dae5f8656cb0733cef4b84964b859dea8a3
Ashley Beecham <ash@momentoussolutions.co.uk>
2019-02-11 14:34:39 +0000

    69329 - Large EmployerModel

---
+++ src/ESFA.DC.ILR.ReferenceDataService.Model/Employers/Employers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ESFA.DC.ILR.ReferenceDataService.Model.Employers
+{
+    public class Employers
+    {
+        public int ERN { get; set; }
+
+        public bool IsLargeEmployer { get; set; }
+
+        public List<LargeEmployer> LargeEmployers { get; set; }
+    }
+}
