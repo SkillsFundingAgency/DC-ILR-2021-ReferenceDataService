f05eb451b53dd3856c116aca60a3add3c48e322d
Ashley Beecham <ash@momentoussolutions.co.uk>
2019-02-12 15:32:52 +0000

    69329 - Removing Large employer flag. The presence of large employer dates will show that the employer is a "large employer"

--- src/ESFA.DC.ILR.ReferenceDataService.Model/Employers/Employers.cs
+++ src/ESFA.DC.ILR.ReferenceDataService.Model/Employers/Employers.cs
@@ -6,8 +6,6 @@ namespace ESFA.DC.ILR.ReferenceDataService.Model.Employers
     {
         public int ERN { get; set; }
 
-        public bool IsLargeEmployer { get; set; }
-
         public List<LargeEmployerEffectiveDates> LargeEmployerEffectiveDates { get; set; }
     }
 }
