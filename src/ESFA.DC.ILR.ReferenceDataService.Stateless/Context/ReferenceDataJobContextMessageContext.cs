1e6488f9269c30cfb8d4bd8155e9931c7daa03dd
Ashley Beecham <ash@momentoussolutions.co.uk>
2019-04-16 08:54:02 +0100

    73499 - JobContextMessageKey updates

--- src/ESFA.DC.ILR.ReferenceDataService.Stateless/Context/ReferenceDataJobContextMessageContext.cs
+++ src/ESFA.DC.ILR.ReferenceDataService.Stateless/Context/ReferenceDataJobContextMessageContext.cs
@@ -1,5 +1,5 @@
-﻿using ESFA.DC.ILR.Constants;
-using ESFA.DC.ILR.ReferenceDataService.Interfaces;
+﻿using ESFA.DC.ILR.ReferenceDataService.Interfaces;
+using ESFA.DC.JobContext.Interface;
 using ESFA.DC.JobContextManager.Model;
 
 namespace ESFA.DC.ILR.ReferenceDataService.Stateless.Context
@@ -15,18 +15,18 @@ namespace ESFA.DC.ILR.ReferenceDataService.Stateless.Context
 
         public string FileReference
         {
-            get => _jobContextMessage.KeyValuePairs[ILRJobContextMessageKeys.Filename].ToString();
-            set => _jobContextMessage.KeyValuePairs[ILRJobContextMessageKeys.Filename] = value;
+            get => _jobContextMessage.KeyValuePairs[JobContextMessageKey.Filename].ToString();
+            set => _jobContextMessage.KeyValuePairs[JobContextMessageKey.Filename] = value;
         }
 
         public string OriginalFileReference
         {
-            get => _jobContextMessage.KeyValuePairs[ILRJobContextMessageKeys.OriginalFilename].ToString();
-            set => _jobContextMessage.KeyValuePairs[ILRJobContextMessageKeys.OriginalFilename] = value;
+            get => _jobContextMessage.KeyValuePairs[JobContextMessageKey.OriginalFilename].ToString();
+            set => _jobContextMessage.KeyValuePairs[JobContextMessageKey.OriginalFilename] = value;
         }
 
-        public string Container => _jobContextMessage.KeyValuePairs[ILRJobContextMessageKeys.Container].ToString();
+        public string Container => _jobContextMessage.KeyValuePairs[JobContextMessageKey.Container].ToString();
 
-        public string OutputReferenceDataFileKey => _jobContextMessage.KeyValuePairs[ILRJobContextMessageKeys.OutputReferenceDataFileKey].ToString();
+        public string OutputReferenceDataFileKey => _jobContextMessage.KeyValuePairs[JobContextMessageKey.IlrReferenceData].ToString();
     }
 }
