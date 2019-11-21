8eaa12ddaed6bc6f10dbb511ad2ec1ee7555bd12
Subcool <peter@subcool>
2019-11-21 14:42:11 +0000

    Stop Loading Ref Data file as tables have been removed.

--- src/IlrReferenceData/ESFA.DC.ILR.ReferenceDataService.ILRReferenceData.Database/zzz.Script.PostDeployment.sql
+++ src/IlrReferenceData/ESFA.DC.ILR.ReferenceDataService.ILRReferenceData.Database/zzz.Script.PostDeployment.sql
@@ -15,34 +15,7 @@ Post-Deployment Script Template
 
 GO
 SET NOCOUNT ON;
-
-RAISERROR('----------------------------------------------------------------------------------------------------------------------------------------------------------------------------',10,1) WITH NOWAIT;
-RAISERROR('		   Populate Staging [ModifiedServerity] with values from Rule table that are Diffrent from the BAU data. This should be the environment specific changes to serverity.',10,1) WITH NOWAIT;
-
-	-- Load Current Serverity Records form ENV into Staging Tables
-	INSERT INTO [Staging].[ModifiedServerity]([Rulename],[Severity])
-	SELECT [Rulename],[Severity]
-	FROM
-	(
-		SELECT TOP 100 PERCENT 
-			'UNION SELECT ''' + MR.[Rulename] + ''' as [Rulename],''' + R.[Severity] + ''' as [Severity]' as [Value], MR.[Rulename], R.[Severity]
-		FROM [Staging].[Rules] MR
-		INNER JOIN [dbo].[Rules] R
-		ON R.[Rulename] = MR.[Rulename]
-		WHERE MR.[Severity] <> R.[Severity]
-		ORDER BY  MR.[Rulename]
-	) as CurrentAmendments
-	WHERE [Rulename] NOT IN (SELECT [Rulename] FROM [Staging].[ModifiedServerity])
-
 GO
-RAISERROR('----------------------------------------------------------------------------------------------------------------------------------------',10,1) WITH NOWAIT;
-
-GO
-
-RAISERROR('		   Ref Data',10,1) WITH NOWAIT;
-	:r .\zReferenceData\Validation.File.Rules.sql
-	:r .\zReferenceData\Validation.Modified.Messages.sql
-	:r .\zReferenceData\Validation.Modified.Serverity.sql
 
 RAISERROR('		   Update User Account Passwords',10,1) WITH NOWAIT;
 GO
