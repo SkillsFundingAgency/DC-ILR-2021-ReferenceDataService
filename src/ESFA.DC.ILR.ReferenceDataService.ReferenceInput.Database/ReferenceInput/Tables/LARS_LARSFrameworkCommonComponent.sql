10ae1cbe60c834cd3f8574df7bc4669f5d352c93
Ashley Beecham <ash@momentoussolutions.co.uk>
2020-03-03 09:02:36 +0000

    Removing LARS Framework table as this is not required for desktop projects. This is the online pre-filtered data variant.

--- src/ESFA.DC.ILR.ReferenceDataService.ReferenceInput.Database/ReferenceInput/Tables/LARS_LARSFrameworkCommonComponent.sql
+++ src/ESFA.DC.ILR.ReferenceDataService.ReferenceInput.Database/ReferenceInput/Tables/LARS_LARSFrameworkCommonComponent.sql
@@ -6,7 +6,6 @@
     [LARS_LARSFrameworkDesktop_Id] INT      NULL,
     [LARS_LARSFramework_Id]        INT      NULL,
     CONSTRAINT [PK_ReferenceInput.LARS_LARSFrameworkCommonComponent] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_ReferenceInput.LARS_LARSFrameworkCommonComponent_ReferenceInput.LARS_LARSFramework_LARSFramework_Id] FOREIGN KEY ([LARS_LARSFramework_Id]) REFERENCES [ReferenceInput].[LARS_LARSFramework] ([Id]),
     CONSTRAINT [FK_ReferenceInput.LARS_LARSFrameworkCommonComponent_ReferenceInput.LARS_LARSFrameworkDesktop_LARSFrameworkDesktop_Id] FOREIGN KEY ([LARS_LARSFrameworkDesktop_Id]) REFERENCES [ReferenceInput].[LARS_LARSFrameworkDesktop] ([Id])
 );
 
