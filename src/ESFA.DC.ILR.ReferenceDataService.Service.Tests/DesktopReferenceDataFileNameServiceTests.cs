55e0a5ad7255c97ee3641fadaa1a096e77c4879e
Ashley Beecham <ash@momentoussolutions.co.uk>
2020-03-24 09:20:14 +0000

    Updating with 1920 changes to filename of FIS ref data

--- src/ESFA.DC.ILR.ReferenceDataService.Service.Tests/DesktopReferenceDataFileNameServiceTests.cs
+++ src/ESFA.DC.ILR.ReferenceDataService.Service.Tests/DesktopReferenceDataFileNameServiceTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
+using ESFA.DC.DateTimeProvider.Interface;
 using ESFA.DC.Logging.Interfaces;
 using FluentAssertions;
 using Moq;
@@ -11,18 +13,21 @@ namespace ESFA.DC.ILR.ReferenceDataService.Service.Tests
     public class DesktopReferenceDataFileNameServiceTests
     {
         [Fact]
-        public void ProcessAync()
+        public async Task ProcessAync()
         {
+            var dateTimeProviderMock = new Mock<IDateTimeProvider>();
+
+            dateTimeProviderMock.Setup(dm => dm.GetNowUtc()).Returns(new System.DateTime(2020, 8, 1, 9, 0, 0));
             var rdsModelVersion = Assembly.GetExecutingAssembly().GetReferencedAssemblies().First(a => a.Name == "ESFA.DC.ILR.ReferenceDataService.Model").Version.ToString(3);
 
-            var expectedFileName = string.Concat(Path.Combine("FilePath", "FileName."), rdsModelVersion, ".zip");
+            var expectedFileName = string.Concat(Path.Combine("FilePath", "FileName."), rdsModelVersion, ".202008010900", ".zip");
 
-            NewService().BuildFileName("FilePath", "FileName").Should().BeEquivalentTo(expectedFileName);
+            NewService(dateTimeProviderMock.Object).BuildFileName("FilePath", "FileName").Should().BeEquivalentTo(expectedFileName);
         }
 
-        private DesktopReferenceDataFileNameService NewService(ILogger logger = null)
+        private DesktopReferenceDataFileNameService NewService(IDateTimeProvider dateTimeProvider = null, ILogger logger = null)
         {
-            return new DesktopReferenceDataFileNameService(logger ?? Mock.Of<ILogger>());
+            return new DesktopReferenceDataFileNameService(dateTimeProvider, logger ?? Mock.Of<ILogger>());
         }
     }
 }
