991e59b4c58ceb2e8a37f468bb4e47cc288d1ef5
Ashley Beecham <ash@momentoussolutions.co.uk>
2020-08-27 15:23:14 +0100

    Removing Learner Model abstraction

--- src/ESFA.DC.ILR.ReferenceDataService.Model/FRM/FrmLearner.cs
+++ src/ESFA.DC.ILR.ReferenceDataService.Model/FRM/FrmLearner.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using ESFA.DC.ILR.ReferenceDataService.Model.Abstract;
 
 namespace ESFA.DC.ILR.ReferenceDataService.Model.FRM
 {
-    public class FrmLearner : AbstractLearner
+    public class FrmLearner
     {
+        public long UKPRN { get; set; }
+
+        public long ULN { get; set; }
+
         public int AimSeqNumber { get; set; }
+
+        public string LearnRefNumber { get; set; }
+
+        public string LearnAimRef { get; set; }
 
         public string LearningAimTitle { get; set; }
 
@@ -14,13 +21,33 @@ namespace ESFA.DC.ILR.ReferenceDataService.Model.FRM
 
         public string PartnerOrgName { get; set; }
 
+        public int? ProgTypeNullable { get; set; }
+
+        public int? StdCodeNullable { get; set; }
+
+        public int? FworkCodeNullable { get; set; }
+
+        public int? PwayCodeNullable { get; set; }
+
+        public DateTime LearnStartDate { get; set; }
+
         public int AimType { get; set; }
 
+        public int FundModel { get; set; }
+
+        public long? PrevUKPRN { get; set; }
+
+        public long? PMUKPRN { get; set; }
+
         public long? PartnerUKPRN { get; set; }
+
+        public string PrevLearnRefNumber { get; set; }
 
         public string SWSupAimId { get; set; }
 
         public DateTime LearnPlanEndDate { get; set; }
+
+        public DateTime? LearnActEndDate { get; set; }
 
         public int? PriorLearnFundAdj { get; set; }
 
--- src/ESFA.DC.ILR.ReferenceDataService.Model/Learner/Learner.cs
+++ src/ESFA.DC.ILR.ReferenceDataService.Model/Learner/Learner.cs
@@ -1,8 +1,33 @@
-﻿using ESFA.DC.ILR.ReferenceDataService.Model.Abstract;
-
-namespace ESFA.DC.ILR.ReferenceDataService.Model.Learner
+﻿namespace ESFA.DC.ILR.ReferenceDataService.Model.Learner
 {
-    public class Learner : AbstractLearner
+    public class Learner
     {
+        public long UKPRN { get; set; }
+
+        public long? PrevUKPRN { get; set; }
+
+        public long? PMUKPRN { get; set; }
+
+        public long ULN { get; set; }
+
+        public string LearnRefNumber { get; set; }
+
+        public string PrevLearnRefNumber { get; set; }
+
+        public string LearnAimRef { get; set; }
+
+        public int? ProgTypeNullable { get; set; }
+
+        public int? StdCodeNullable { get; set; }
+
+        public int? FworkCodeNullable { get; set; }
+
+        public int? PwayCodeNullable { get; set; }
+
+        public DateTime LearnStartDate { get; set; }
+
+        public DateTime? LearnActEndDate { get; set; }
+
+        public int FundModel { get; set; }
     }
 }
